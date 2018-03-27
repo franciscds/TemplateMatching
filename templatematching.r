bdf5ff6b3a6d2632cc60050cedcc2939f0ad1eaf
Francis Santos <franciscarlossantos@gmail.com>
2018-03-26 23:24:14 -0300

    Update templatematching.r

--- templatematching.r
+++ templatematching.r
@@ -1,36 +1,36 @@
+#setwd("C:/Users/fcdossantos/Desktop/UFMG 201801/Reconhecimento de Padrões/Template Matching")
 rm(list=ls())
 graphics.off()
-# install.packages("jpeg")
+rm(list=ls())
+graphics.off()
+# ++++++++++install.packages("jpeg")
 # install.packages("bmp")
-# install.packages("pixmao")
-require(grDevices)
-library('jpeg')
-library('bmp')
-library('pixmap')
-# library('rgl')
+# install.packages("pixmap")
+# #require(grDevices)
+ library('jpeg')
+ library('bmp')
+ library('pixmap')
+ # #ibrary('rgl')
+ library('lattice')
 
 rotate <- function(x) t(apply(x, 2, rev))
 
-FeatureExtraction <- function(x) 
+extrairFeatures <- function(img_vec) 
 {
-    vetor_features <- vector(mode = "numeric")
-    vetor_features <- c(vetor_features, sum(sum(as.numeric(x)))) ## nъmero de 1s
-
-    x_esquerda <- x[, 1:dim(x)[2]/2]
-    x_direita <- x[, (dim(x)[2]/2):dim(x)[2]]
-    x_cima <- x[1:(dim(x)[1]/2), ]
-    x_baixo <- x[(dim(x)[1]/2):dim(x)[1], ]
-    vetor_features <- c(vetor_features, sum(sum(as.numeric(x_esquerda)))) ## 
-    vetor_features <- c(vetor_features, sum(sum(as.numeric(x_direita)))) ## 
-    vetor_features <- c(vetor_features, sum(sum(as.numeric(x_cima)))) ## 
-    vetor_features <- c(vetor_features, sum(sum(as.numeric(x_baixo)))) ## 
-  
+  #img_mat <- makematrix(img_vec)
+  f = matrix(0,3,1)
+  f[1,1] = sum(img_vec)
+  f[2,1] = var(img_vec)
+  f[3,1] = sd(img_vec)
+  f
 }
 
-similarityBetween2Images <- function(char1,char2)
-{
-  
+makematrix <- function(vet)
+{ 
+  m <-matrix(vet,nrow=56, ncol=46)
+  m
 }
+
 
 #Leitura de dados ###################################################################################
 xt <- c();
@@ -38,7 +38,7 @@ xe <- c();
 
 for(i in 1:5){
   
-    for(j in 1:5){
+  for(j in 1:5){
     
     path_xt <- paste(getwd(), paste(paste("/f", i, sep=""), paste(paste("teste",j, sep=""),".bmp", sep=""), sep="") ,sep="");
     path_xe <- paste(getwd(), paste(paste("/F", i, sep=""), paste(paste("TESTE",j,sep=""),"R.BMP",sep=""),sep=""),sep="");
@@ -59,15 +59,29 @@ ye <- matrix(seq(1,5,1), ncol=1, nrow=25);
 image(matrix(xe[1,],nrow = 56,ncol=46))
 image(matrix(xe[10,],nrow = 56,ncol=46))
 
+#extrai features
+
+n_features = 4;
+features = matrix(seq(1,5,1), ncol=3, nrow=25)
+
 for( i in 1:25){
-    
-    for(j in 1:25){
-        
-        m <- M[j:(j + 43 - 1), i:(i + 32 - 1)]
-        fm <- features(m)
-        fK <- features(K)
-        Mcorr[j, i] <- sim(fK, fm)
-  }
-  
+  features[i,] = extrairFeatures(xe[i,])
 }
+
+FacesID <- c(seq(1:5), seq(1:5), seq(1:5), seq(1:5), seq(1:5))
+
+dat <- data.frame(FacesID, Media=features[,1], Variancia=features[,2])
+
+xyplot(Variancia ~ Media, groups=dat$FacesID, data = dat, cex=2)
+
+
+
+
+
+
+
+
+
+
+
 
